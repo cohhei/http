c28a4de35e88ea8b298a416c988f05656d0bdc33
Kohei Kimura <kohei.kimura@nulab.com>
2020-02-01 14:14:05 +0900

    udp client

---
+++ udp.go
@@ -0,0 +1,58 @@
+package http
+
+import (
+	"io"
+	"syscall"
+)
+
+// UDPClient is an interface for the UDP client
+type UDPClient interface {
+	// SendTo sends data.
+	SendTo(data []byte, addr *Addr) error
+
+	// Recvfrom returns io.Reader
+	Recvfrom() (io.Reader, error)
+
+	// Close closes the connection.
+	Close() error
+}
+
+// NewUDPClient creates a client
+func NewUDPClient() UDPClient {
+	return &udpClient{}
+}
+
+type udpClient struct {
+	socket int
+}
+
+func (c *udpClient) SendTo(data []byte, addr *Addr) error {
+	socket, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_DGRAM, 0)
+	if err != nil {
+		return err
+	}
+	c.socket = socket
+
+	return syscall.Sendto(socket, data, 0, addr.sockaddr())
+}
+
+func (c *udpClient) Recvfrom() (io.Reader, error) {
+	return &receiver{c.socket}, nil
+}
+
+func (c *udpClient) Close() error {
+	return syscall.Close(c.socket)
+}
+
+type receiver struct {
+	socket int
+}
+
+func (r *receiver) Read(buf []byte) (int, error) {
+	n, _, err := syscall.Recvfrom(r.socket, buf, 0)
+	if err != nil {
+		return 0, err
+	}
+
+	return n, io.EOF
+}
---
+++ udp_test.go
@@ -0,0 +1,33 @@
+package http
+
+import (
+	"io/ioutil"
+	"testing"
+)
+
+func TestUDPClient(t *testing.T) {
+	c := NewUDPClient()
+	addr := &Addr{
+		IP:   [4]byte{8, 8, 8, 8},
+		Port: 53,
+	}
+
+	b := []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x07, 0x65, 0x78, 0x61, 0x6d, 0x70, 0x6c, 0x65, 0x03, 0x63, 0x6f, 0x6d, 0x00, 0x00, 0x01, 0x00, 0x01}
+	if err := c.SendTo(b, addr); err != nil {
+		t.Fatal(err)
+	}
+	defer c.Close()
+
+	r, err := c.Recvfrom()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	resp, err := ioutil.ReadAll(r)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(resp) == 0 {
+		t.Fatal("response is empty")
+	}
+}
