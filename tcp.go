0c22ca7602a45ee6f8962c371fc4090908a71023
Kohei Kimura <kohei.kimura@nulab.com>
2020-02-09 16:13:42 +0900

    tcp listener

--- tcp.go
+++ tcp.go
@@ -12,12 +12,10 @@ type TCPClient interface {
 	// The returned connection should be closed.
 	Connect(ip [4]byte, port int) (connection io.ReadWriteCloser, err error)
 
-	// Listen listens for TCP connections.
-	// This method creates a new socket and binds it to the given address by using syscall.Bind().
-	// In addition, calls syscall.Listen() and syscall.Accept().
-	// The process will be blocked until a request comes.
-	// The returned connection should be closed.
-	Listen(ip [4]byte, port int) (connection io.ReadWriteCloser, err error)
+	// Listen listens for TCP connections and returns a listener.
+	// This method creates a new socket and binds it to the given address by using syscall.Bind() and syscall.Listen().
+	// The returned listener should be closed.
+	Listen(ip [4]byte, port int) (TCPListener, error)
 }
 
 // NewTCPClient creates a client
@@ -41,12 +39,11 @@ func (c *tcpClient) Connect(ip [4]byte, port int) (io.ReadWriteCloser, error) {
 	return &tcpConnection{socket, sa}, nil
 }
 
-func (c *tcpClient) Listen(ip [4]byte, port int) (io.ReadWriteCloser, error) {
+func (c *tcpClient) Listen(ip [4]byte, port int) (TCPListener, error) {
 	socket, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_STREAM, 0)
 	if err != nil {
 		return nil, err
 	}
-	defer syscall.Close(socket)
 
 	if err := syscall.Bind(socket, sockaddr(ip, port)); err != nil {
 		return nil, err
@@ -56,12 +53,34 @@ func (c *tcpClient) Listen(ip [4]byte, port int) (io.ReadWriteCloser, error) {
 		return nil, err
 	}
 
-	nfd, sa, err := syscall.Accept(socket)
+	return &tcpListener{socket}, nil
+}
+
+// TCPListener is an interface for TCP servers
+type TCPListener interface {
+	// Accept waits connection requests from clients.
+	// The process will be blocked until a request comes.
+	Accept() (connection io.ReadWriteCloser, err error)
+
+	// Close closes the socket.
+	Close() error
+}
+
+type tcpListener struct {
+	socket int
+}
+
+func (l *tcpListener) Accept() (connection io.ReadWriteCloser, err error) {
+	nfd, sa, err := syscall.Accept(l.socket)
 	if err != nil {
 		return nil, err
 	}
 
 	return &tcpConnection{nfd, sa}, nil
+}
+
+func (c *tcpListener) Close() error {
+	return syscall.Close(c.socket)
 }
 
 type tcpConnection struct {
