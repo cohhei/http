a5d2950a03e80f399a8ce2c5d558c4cbb9a8c307
Kohei Kimura <kohei.kimura@nulab.com>
2020-02-13 20:10:07 +0900

    use response writer and header

--- examples/server/server.go
+++ examples/server/server.go
@@ -4,25 +4,41 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"os"
 
 	"github.com/cohhei/http"
 )
 
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
-	http.HandleFunc("/", func(w io.Writer, r *http.Request) {
-		w.Write([]byte("HTTP/1.1 200 OK\n\nHello World!"))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World!"))
 	})
-	http.HandleFunc("/index.html", func(w io.Writer, r *http.Request) {
-		w.Write([]byte("HTTP/1.1 200 OK\n\nindex.html"))
+	http.HandleFunc("/index.html", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("index.html"))
 	})
-	http.HandleFunc("/ua", func(w io.Writer, r *http.Request) {
-		w.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\n\n%s", r.Header["User-Agent"])))
+	http.HandleFunc("/png", func(w http.ResponseWriter, r *http.Request) {
+		h := w.Header()
+		h["Content-Type"] = "image/png"
+
+		f, err := os.Open("./examples/server/200x200.png")
+		if err != nil {
+			fmt.Fprintf(w, err.Error())
+			return
+		}
+		defer f.Close()
+		io.Copy(w, f)
 	})
-	log.Print("http://127.0.0.1:8080/")
-	log.Print("http://127.0.0.1:8080/index.html")
-	log.Print("http://127.0.0.1:8080/ua")
-	if err := http.ListenAndServe(8080); err != nil {
+	http.HandleFunc("/ua", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(fmt.Sprintf("%s", r.Header["User-Agent"])))
+	})
+
+	port := 8080
+	log.Print(fmt.Sprintf("http://127.0.0.1:%d/", port))
+	log.Print(fmt.Sprintf("http://127.0.0.1:%d/index.html", port))
+	log.Print(fmt.Sprintf("http://127.0.0.1:%d/png", port))
+	log.Print(fmt.Sprintf("http://127.0.0.1:%d/ua", port))
+	if err := http.ListenAndServe(port); err != nil {
 		log.Fatal(err)
 	}
 }
